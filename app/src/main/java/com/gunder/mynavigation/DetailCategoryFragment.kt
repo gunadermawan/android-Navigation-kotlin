ce773457b779ab4e9a2e59182147df3f9b6eb53d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-27 16:37:16 +0700

    mnmbhkan argument dari deeplink dan animation

--- app/src/main/java/com/gunder/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/gunder/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.gunder.mynavigation.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -34,7 +35,12 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDesc"
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
