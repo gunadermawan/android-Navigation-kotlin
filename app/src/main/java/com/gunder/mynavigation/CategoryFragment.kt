6da4111ea27e685317545f029a82c0437291b45d
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-09-27 12:46:24 +0700

    kode untuk mengrim data dengan bundle update

--- app/src/main/java/com/gunder/mynavigation/CategoryFragment.kt
+++ app/src/main/java/com/gunder/mynavigation/CategoryFragment.kt
@@ -5,23 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.gunder.mynavigation.databinding.FragmentCategoryBinding
 class CategoryFragment : Fragment() {
-    private var _binding:   FragmentCategoryBinding? = null
+    private var _binding: FragmentCategoryBinding? = null
     private val binding get() = _binding!!
 
-   companion object {
-       val EXTRA_NAME = "extra_name"
-       val EXTRA_STOCK = "extra_stock"
-   }
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_STOCK = "extra_stock"
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         _binding = FragmentCategoryBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCategoryLifecycle.setOnClickListener { _ ->
+            val mBundle = Bundle()
+            mBundle.putString(EXTRA_NAME,"Ini adalah data yang dikirim")
+            mBundle.putLong(EXTRA_STOCK, 9)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment,mBundle)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
